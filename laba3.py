87874d3e4bf16625dc6f12953fe67d075389e50b
sofakob <hhitech628@gmail.com>
2025-05-30 15:33:11 +0300

    chitaet x1, x2, ..., xn

--- laba3.py
+++ laba3.py
@@ -20,34 +20,74 @@ def rivnain(alhpa:int, s:list, n:int):
             div.append(0)
     for i in range(len(div)):
         if div[i]!=0:
-            return k, div
+            return int(k), div
     return rivnain(alhpa, s, n)
 
 def huba(alhpa:int, s:list, n:int, k:list, div:list):
     k2, div2=rivnain(alhpa, s, n)
-    k.append(k2)
-    div.append(div2)
+    if k2 not in k:
+            k.append(k2)
+            div.append(div2)
+        
+    
     return k, div
 
-
+def liniyno_nezalegn(k, div):
+    k_for=k[:]
+    div_for=div[:]
+    matr=numpy.array([row +[k[i]] for i, row in enumerate(div)])
+    matrixa=[row +[k[i]] for i, row in enumerate(div)]
+    matr_without_linia=numpy.array(matrixa[0])
+    matr_without_linia=numpy.array(matrixa[0:1])
+    index1=numpy.linalg.matrix_rank(matr_without_linia, 0)
+    for i in range(1, len(k_for)):
+        matr_without_linia=numpy.array(matrixa[0:i+1])
+        index2=numpy.linalg.matrix_rank(matr_without_linia, 1e-10)
+        if index2==index1:
+            matrixa.pop(i)
+            k_for.pop(i)
+            div_for.pop(i)
+            i=-1
+        index1=index2
+    matr_without_linia=numpy.array(matrixa[:])
+    
+    return k_for, div_for
+    
 
 
 def index_calculus(alhpa:int, beta:int, n:int):
     c=3.38
     base=int(2)
-    b=c*math.exp((1/2)*math.sqrt(math.log(n, base)*math.log(math.log(n, base), base)))
+    b=int(c*math.exp((1/2)*math.sqrt(math.log(n, base)*math.log(math.log(n, base), base))))
     s=list(sympy.primerange(2, b)) 
+    print(s)
+
     k, div=rivnain(alhpa, s, n)
     k2, div2=rivnain(alhpa, s, n)
+    while k==k2:
+        k2, div2=rivnain(alhpa, s, n)
     k=[k, k2]
     div=[div, div2]
+    print(k, div)
     reshenie=None
-    while reshenie==None:
+    i=0
+    while reshenie is None:
         try:
-            reshenie=numpy.linalg.solve(k, div)
+            k, div=liniyno_nezalegn(k, div)
+            matr=sympy.Matrix([row +[k[i]] for i, row in enumerate(div)])
+            k_coef=[]
+            for i in range(len(k)):
+                k_coef.append("X"+str(i+1))
+
+            reshenie = sympy.solve_linear_system_LU(matr, k_coef)
         except:
             reshenie=None
             k, div=huba(alhpa, s, n, k, div)
+    #print(reshenie)
+
+ 
+
+        
         
         
 
@@ -62,4 +102,5 @@ def index_calculus(alhpa:int, beta:int, n:int):
 
 
 
-index_calculus(1, 1, int(1000))
+
+index_calculus(10, int(17), int(47))
